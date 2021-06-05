278d591ef5fcd07f2789dd42033b7eb35e87d9e4
DenizNazari <74727948+DenizNazari@users.noreply.github.com>
2021-06-05 20:33:45 +0300

    Merge branch 'main' of https://github.com/PowerPuffGirlss/CRM into main

---
+++ crm_urunler.py
@@ -0,0 +1,50 @@
+import database
+
+database.database_ac("csv")
+
+# stok mallar , markalar , urun turu , urun 
+#  satış temsilcileri , musteri, Teklifleriniz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# database.database_ac("Stok Yönetimi")
+# database.database_ac("Satış Yönetimi")
+# database.database_ac("Ürün Yönetimi")
+
+
+
+
+
