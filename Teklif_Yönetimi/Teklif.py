458290fd4aab7614999f649368ee6f9a7f3b0cbb
Özge Cengizhan <74068525+ozgecengizhan@users.noreply.github.com>
2021-06-05 12:57:25 +0300

    T

--- Teklif_Yönetimi/Teklif.py
+++ Teklif_Yönetimi/Teklif.py
@@ -1,14 +1,18 @@
 import mysql.connector
 
 mydb=mysql.connector.connect(
-    host="localhost", 
+    host="127.0.0.1" ,
 
     user="root", 
 
-    password="08Ozge34"
+    password="08Ozge34",
+
+    auth_plugin="mysql_native_password"
+
 )
-print(mydb)
 
 mycursor=mydb.cursor()
+
+mycursor.execute("CREATE DATABASE Teklif ")
 
 mycursor.execute("Create Table customers (name Varchar(255),adress varchar(255))")
